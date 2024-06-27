950c1749e9d1e6f87cdda023b3c399eedf4a2304
shige99 <shige992306@gmail.com>
2024-06-27 19:31:22 +0900

    add gnome_sort.py

---
+++ gnome_sort.py
@@ -0,0 +1,19 @@
+from typing import List
+import random
+
+def gnome_sort(numbers: List[int]) -> List[int]:
+    len_numbers = len(numbers)
+    index = 0
+    while index < len_numbers:
+        if index == 0:
+            index = index + 1
+        if numbers[index] >= numbers[index-1]:
+            index = index + 1
+        else:
+            numbers[index], numbers[index-1] = numbers[index+1], numbers[index]
+            index -= 1
+    return numbers
+
+if __name__ == '__main__':
+    nums = [random.randint(0,1000) for _ in range(10)]
+    print(gnome_sort(nums))
